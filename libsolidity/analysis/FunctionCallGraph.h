cfaacfc7c1a7b86a7ff4c646d193aa24ad6aac41
chriseth <chris@ethereum.org>
2022-02-10 19:16:10 +0100

    User-defined literal suffixes.

--- libsolidity/analysis/FunctionCallGraph.h
+++ libsolidity/analysis/FunctionCallGraph.h
@@ -76,6 +76,7 @@ private:
 	bool visit(UnaryOperation const& _unaryOperation) override;
 	bool visit(ModifierInvocation const& _modifierInvocation) override;
 	bool visit(NewExpression const& _newExpression) override;
+	bool visit(Literal const& _literal) override;
 
 	void enqueueCallable(CallableDeclaration const& _callable);
 	void processQueue();
