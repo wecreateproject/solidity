6beb1f2e74423dca03e8b12f1543421dac5e6bb7
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-02-07 12:03:46 +0100

    fixup! User-defined operators: Tests

--- test/libsolidity/semanticTests/operators/userDefined/calling_operator_checked.sol
+++ test/libsolidity/semanticTests/operators/userDefined/calling_operator_checked.sol
@@ -4,12 +4,6 @@ function checkedAdd(U8 x, U8 y) pure returns (U8) {
     return U8.wrap(U8.unwrap(x) + U8.unwrap(y));
 }
 
-function uncheckedAdd(U8 x, U8 y) pure returns (U8) {
-    unchecked {
-        return U8.wrap(U8.unwrap(x) + U8.unwrap(y));
-    }
-}
-
 contract C {
     using {checkedAdd as +} for U8;
 
@@ -23,24 +17,6 @@ contract C {
         }
     }
 }
-
-contract D {
-    using {uncheckedAdd as +} for U8;
-
-    function testUncheckedOperator() public pure returns (U8) {
-        return U8.wrap(250) + U8.wrap(10);
-    }
-
-    function testUncheckedOperatorInUncheckedBlock() public pure returns (U8) {
-        unchecked {
-            return U8.wrap(250) + U8.wrap(10);
-        }
-    }
-}
-
-contract E is C, D {}
 // ----
 // testCheckedOperator() -> FAILURE, hex"4e487b71", 0x11
 // testCheckedOperatorInUncheckedBlock() -> FAILURE, hex"4e487b71", 0x11
-// testUncheckedOperator() -> 4
-// testUncheckedOperatorInUncheckedBlock() -> 4
